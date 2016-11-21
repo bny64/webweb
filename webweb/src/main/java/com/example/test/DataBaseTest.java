a102c883b77f41ccbe86480f3aecce33014bc13c
8-814-01 <8-814-01@24_816_8>
2016-11-21 11:41:32 +0900

    ?

---
+++ webweb/src/main/java/com/example/test/DataBaseTest.java
@@ -0,0 +1,18 @@
+package com.example.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.config.ApplicationConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { ApplicationConfig.class })
+public class DataBaseTest {
+
+	
+
+}
